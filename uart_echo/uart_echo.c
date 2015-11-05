a343e0e74bbbce8b3cdc2e94ff720499aa003fd9
Carlos Neri <carlosneri86@gmail.com>
2015-11-05 15:34:50 -0800

    [ADD] Added a simple uart sample code

---
+++ uart_echo/uart_echo.c
@@ -0,0 +1,75 @@
+#include <stdio.h>   /* Standard input/output definitions */
+#include <string.h>  /* String function definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
+#include <stdint.h> 
+#include <stdlib.h>
+ 
+#define APPLICATION_BUFFER_SIZE	(50)
+
+const int8_t SerialHelloWorld[] =
+{
+    "Hello From UART"
+};
+
+int main (void)
+{
+	int32_t  SerialPort;	
+	struct termios options;
+	uint8_t * SerialBuffer;
+
+
+	printf("\nInit serial port sample code.....\n");
+	/* Open the UART3 driver as read/write and non blocking*/
+	SerialPort = open("/dev/ttymxc2",O_RDWR|O_NOCTTY|O_NDELAY);
+
+
+	if(SerialPort  > 0)
+	{
+		printf("TTY opened!!\n");
+		/* Configure the UART */	
+		fcntl(SerialPort, F_SETFL , 0);
+		/* Baudrate set to 9600 */
+		tcgetattr(SerialPort,&options);
+		cfsetispeed(&options,B9600);
+		cfsetospeed(&options,B9600);
+		/* Enable receiver */
+		options.c_cflag |= (CLOCAL | CREAD);
+		/* No parity, 8 bits, one stop */
+		options.c_cflag &= ~PARENB;
+		options.c_cflag &= ~CSTOPB;
+		options.c_cflag &= ~CSIZE;
+		options.c_cflag |= CS8;
+		/* Set the configuration*/
+		tcsetattr(SerialPort, TCSANOW, &options);
+
+	}
+	else
+	{
+		/* In case the UART wasn't available		*/
+		/* use perror to print the issue		*/
+		/* If the issue is "permission denied", then, 	*/
+		/* change the ttymxc2 permissons to RW          */
+		/* and then try again				*/	
+		perror("Serial");
+		return(-1);
+	}
+
+	SerialBuffer = (uint8_t*)malloc(APPLICATION_BUFFER_SIZE); 
+		
+	(void)write(SerialPort,&SerialHelloWorld[0],sizeof(SerialHelloWorld));
+		
+
+    (void)read(SerialPort,SerialBuffer,sizeof(SerialHelloWorld));
+
+    printf("\nReceived: %s",SerialBuffer);
+
+	close(SerialPort);
+	free(SerialBuffer);
+
+	return(1);
+
+}
+
